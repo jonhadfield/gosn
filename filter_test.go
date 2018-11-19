90ace35bde535ef93733d10db3edb3cc2df1d2fa
Jon Hadfield <jon@lessknown.co.uk>
2018-11-19 21:31:45 +0000

    add filter tests.

--- filter_test.go
+++ filter_test.go
@@ -4,33 +4,38 @@ import (
 	"testing"
 )
 
-//func getTestAnimalNotes() []Item {
-//	dogNote := createNote("Dogs", "Can't look up")
-//	gnuNote := createNote("GNU", "Is not Unix")
-//	spiderNote := createNote("Spiders", "Are not welcome")
-//	return []Item{*dogNote, *gnuNote, *spiderNote}
-//}
-//
-//func getTestFoodNotes() []Item {
-//	cheeseNote := createNote("Cheese", "Is not a vegetable")
-//	baconNote := createNote("Bacon", "Goes with everything")
-//	return []Item{*cheeseNote, *baconNote}
-//}
-
 func TestFilterNoteTitle(t *testing.T) {
 	gnuNote := createNote("GNU", "Is not Unix")
 	filter := Filter{
-		Type:"Note",
-		Key: "Title",
-		Comparison:"==",
-		Value: "GNU",
+		Type:       "Note",
+		Key:        "Title",
+		Comparison: "==",
+		Value:      "GNU",
 	}
 	itemFilters := ItemFilters{
-		Filters: []Filter{filter},
-		MatchAny:true,
+		Filters:  []Filter{filter},
+		MatchAny: true,
 	}
 	res := applyNoteFilters(*gnuNote, itemFilters, nil)
 	if ! res {
 		t.Error("failed to match note by title")
+	}
+}
+
+func TestFilterTagTitle(t *testing.T) {
+	gnuNote := createTag("GNU")
+	filter := Filter{
+		Type:       "Tag",
+		Key:        "Title",
+		Comparison: "==",
+		Value:      "GNU",
+	}
+	itemFilters := ItemFilters{
+		Filters:  []Filter{filter},
+		MatchAny: true,
+	}
+	res := applyTagFilters(*gnuNote, itemFilters)
+	if ! res {
+		t.Error("failed to match tag by title")
 	}
 }
