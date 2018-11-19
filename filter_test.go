d2bb43a6246676c168da7dfd9833efebb0c324bf
Jon Hadfield <jon@lessknown.co.uk>
2018-11-19 21:45:21 +0000

    add note text regex test.

--- filter_test.go
+++ filter_test.go
@@ -23,7 +23,7 @@ func TestFilterNoteTitle(t *testing.T) {
 }
 
 func TestFilterTagTitle(t *testing.T) {
-	gnuNote := createTag("GNU")
+	gnuTag := createTag("GNU")
 	filter := Filter{
 		Type:       "Tag",
 		Key:        "Title",
@@ -34,8 +34,44 @@ func TestFilterTagTitle(t *testing.T) {
 		Filters:  []Filter{filter},
 		MatchAny: true,
 	}
-	res := applyTagFilters(*gnuNote, itemFilters)
+	res := applyTagFilters(*gnuTag, itemFilters)
 	if ! res {
 		t.Error("failed to match tag by title")
+	}
+}
+
+func TestFilterNoteText(t *testing.T) {
+	gnuNote := createNote("GNU", "Is not Unix")
+	filter := Filter{
+		Type:       "Tag",
+		Key:        "Text",
+		Comparison: "==",
+		Value:      "Is not Unix",
+	}
+	itemFilters := ItemFilters{
+		Filters:  []Filter{filter},
+		MatchAny: true,
+	}
+	res := applyTagFilters(*gnuNote, itemFilters)
+	if ! res {
+		t.Error("failed to match note by text")
+	}
+}
+
+func TestFilterNoteTextByRegex(t *testing.T) {
+	gnuNote := createNote("GNU", "Is not Unix")
+	filter := Filter{
+		Type:       "Tag",
+		Key:        "Text",
+		Comparison: "~",
+		Value:      "^.*Unix",
+	}
+	itemFilters := ItemFilters{
+		Filters:  []Filter{filter},
+		MatchAny: true,
+	}
+	res := applyTagFilters(*gnuNote, itemFilters)
+	if ! res {
+		t.Error("failed to match note by text regex")
 	}
 }
