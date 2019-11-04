48770b14a8e989f3d0f11a2d8170a267da7d372a
Jon Hadfield <jon@lessknown.co.uk>
2019-11-04 18:35:39 +0000

    reintroduce logging due to dependency from other apps.

--- logging.go
+++ logging.go
@@ -3,3 +3,13 @@ package gosn
 type Logger func(...interface{})
 
 var debugLog Logger
+
+func SetDebugLogger(logger Logger) {
+	debugLog = logger
+}
+
+var errorLog Logger
+
+func SetErrorLogger(logger Logger) {
+	errorLog = logger
+}
